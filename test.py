835cf0a32b51ac5321019a1cb11b4a95d45d0122
jay <jeanjay.quitayen@asti.dost.gov.ph>
2020-06-15 18:44:24 +0000

    modified

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
+#!/usr/bin/python3
 import eel
 import lirc
 
 eel.init("web")
 
 def getIrKeyPress():
-    while True:
-        code = lirc.nextcode()
-        if len(code) > 0:
-            code = code[0]
-            eel.getKeyPressed(code)
+    code = lirc.nextcode()
+    if len(code) > 0:
+        code = code[0]
+        eel.getKeyPressed(code)
 
 #eel.spawn(getIrKeyPress)
-eel.start("index.html",block=False)
+eel.start("index.html",block=False,cmdline_args=["--start-fullscreen"])
 print("Running rural")
-socketid = lirc.init("test",blocking=False)
-lirc.set_blocking(False,socketid)
+socketid = lirc.init("test")
+#lirc.set_blocking(False,socketid)
 eel.sleep(5.0)
 print("running IR")
 while True:
