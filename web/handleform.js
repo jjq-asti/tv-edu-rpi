2edc2aafdaa357244b649bf39b6ae7431c063f89
jay <robot-1>
2020-06-17 09:19:59 +0800

    added submit

--- web/handleform.js
+++ web/handleform.js
@@ -2,7 +2,17 @@
 
 var counter = 1;
 var selectionCounter = [0,0,0];
+var q_completed = 0;
 const choices = ['option-11','option-12','option-13','option-14']
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
  function displayAnswer1() {
   if (document.getElementById('option-11').checked) {
     // document.getElementById('block-11').style.border = '3px solid limegreen'
@@ -59,8 +69,15 @@ const choices = ['option-11','option-12','option-13','option-14']
         q3_current = 'option-14'
     }
   }
+  sub_btn = document.getElementById("completed");
+  if (q_completed !== 3){
+      q_completed = counter;
+      sub_btn.innerHTML = q_completed;
+  }
+  
   if (q1_current !== 0 && q2_current !== 0 && q3_current !== 0){
-    document.getElementById("submit").style.display = "inline-block";
+    sub_btn = document.getElementById("submit");
+    sub_btn.disabled = false;
 }
 }
 
@@ -100,6 +117,8 @@ var q3_current = 0
 var q3_prev = 0;
 
 function fetchQuestion1(){
+    sub_btn = document.getElementById("submit");
+    sub_btn.disabled = true;
     q_element = document.getElementById("question")
     q_11 = document.getElementById("option-label-11")
     q_12 = document.getElementById("option-label-12")
@@ -201,10 +220,21 @@ function fetchPrevQuestion(){
 
 
 function submit(){
-    a1 = document.getElementById(q1_current).value
-    a2 = document.getElementById(q2_current).value
-    a3 = document.getElementById(q3_current).value
-    console.log(a1,a2,a3)
+    var a1 = document.getElementById(q1_current).value
+    var a2 = document.getElementById(q2_current).value
+    var a3 = document.getElementById(q3_current).value
+    var details = document.getElementsByClassName("student-details")
+    var stn = document.getElementById("student_number_value")
+    var modal_message = document.getElementById("modal_message")
+    if (isNaN(parseInt(stn))){
+        modal_message.innerHTML = "Student Number Missing!"
+       
+    }else{
+        modal_message.innerHTML = "Please verify your answer before proceding."
+    }
+    modal.style.display = "block";
+    
+    console.log(a1,a2,a3,details[0].innerHTML,stn.innerHTML)
 }
 
 function incrementSelectionCounter(counter){
@@ -261,6 +291,8 @@ function getKeyPressed(key){
 	fetchNextQuestion();
     }else if ( key === "key_left" ) {
 	fetchPrevQuestion();
+    }else if (key === "ok") {
+    submit();
     }else if ( mode === "INPUT_ID" ){
         if ( !(isNaN(parseInt(key.slice(-1)))) ){
             el = document.getElementById("studentID");
@@ -270,3 +302,26 @@ function getKeyPressed(key){
         }
     }
 }
+
+
+
+// Get the modal
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function hide_modal() {
+  console.log("closing")
+  modal.style.display = "none";
+}
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
